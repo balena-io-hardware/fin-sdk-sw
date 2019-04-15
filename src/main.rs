0d2187acd96b1c6f66247c0ffa18d0a28198a23f
Zahari Petkov <zahari@balena.io>
2019-04-16 00:08:04 +0300

    Printing balenaFin hardware version
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 extern crate clap;
 
+#[macro_use]
+extern crate nix;
+
 mod command;
+mod version;
 
 use command::{get_command, Command};
+use version::print_version;
 
 fn main() {
     match get_command() {
-        Command::Version => println!("1.0"),
+        Command::Version => print_version(),
     }
 }
