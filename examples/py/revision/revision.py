7800581874365ad28fab06676f6dc4dfe1165b60
Zahari Petkov <zahari@balena.io>
2019-08-04 14:28:53 +0300

    Examples with Dockerfiles
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- examples/py/revision/revision.py
+++ examples/py/revision/revision.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import signal
 import gi
 
 gi.require_version("Fin", "0.1")
@@ -12,3 +13,4 @@ print("Revision (props):", fin.props.revision)
 
 print("Revision (method):", fin.get_revision())
 
+signal.pause()
--- revision.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-Fin = gi.require('Fin', '0.1')
-
-const fin = new Fin.Client()
-
-console.log(fin.version)
